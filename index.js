024cfd305b1de841a95c1608643fc2407998c1f0
bbabou94 <bbabou94@gmail.com>
2018-12-21 03:45:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // add solution here
+
+function theBeatlesPlay(){
+const musician = [4];
+  
+  musician[0] = "John Lennon";
+  musician[1] = "Paul McCartney";
+  musician[2] = "George Harrison";
+  musician[3] = "Ringo Starr";
+  
+  const instrument = [4];
+  
+  instrument[0] = "Guitar";
+  instrument[1] = "Bass Guitar";
+  instrument[2] = "Lead Guitar";
+  instrument[3] = "Drums";
+  var text = "";
+  var n;
+    for(n = 0; n<=3; n++)
+      text += musician[n] +" plays " + instrument[n];
+}
+
+
+var n = 3;
+const facts = [n]
+  facts[0] = "He was the last Beatle to learn to drive";
+  facts[1] = "He was never a vegetarian";
+  facts[2] = "He was a choir boy and boy scout";
+  facts[3] = "He hated the sound of his own voice";
+  
+  function johnLennonFacts(facts){
+  //String exclamation = "!!!";
+  
+    //while (facts[n] + "!!!"){
+  console.log(facts[n] + "!!!");
+   n++;
+    
+  
+    }
